6fd107173ec31c586101216780c82bd63a47154f
visdxebia <vishal.daga@xebia.com>
2023-05-23 00:42:30 +0530

    sdf

--- postDeploy.js
+++ postDeploy.js
@@ -26,48 +26,50 @@ exec(`aws lakeformation put-data-lake-settings --cli-input-json file://lakePermi
 
 const adminUiS3BucketName = getAdminUiBucketName(envName);
 
-exec(`aws s3api head-object --bucket ${adminUiS3BucketName} --key admin_ui/index.html`, (error, exists)=>{
-  if (!exists){
-    // Use IDP Config template to generate right idp config and replace idpConfig.js in adminUi bundle
-    let idpConfigTemplateContents = fs.readFileSync("./adminUiIdpConfigTemplate.js", "utf-8");
-
-    const cognitoStackExportsStartKey = 'PlatformPhaseSetupStackClientOnboardingUiStack';
-    const cognitoStackExportsKey = Object.keys(stackExports).filter(k => k.startsWith(cognitoStackExportsStartKey));
-    const cognitoExports = stackExports[cognitoStackExportsKey];
-
-    const apiGatewayStackExportsStartKey = 'PlatformPhaseSetupStackClientOnboardingUiStack';
-    const apiGatewayStackExportsKey = Object.keys(stackExports).filter(k => k.startsWith(apiGatewayStackExportsStartKey));
-    const apiGatewayExports = stackExports[apiGatewayStackExportsKey];
-
-    const clientUserPoolID = cognitoExports[`Export${getExportName('userPoolId', {id: `spiadmin${envName}`})}`];
-    const clientUserPoolWebClientId = cognitoExports[`Export${getExportName('userPoolClient', {id: `spiadmin${envName}`})}`];
-    idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)userPoolId:.*/, `$1userPoolId: "${clientUserPoolID}",`);
-    idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)userPoolWebClientId:.*/, `$1userPoolWebClientId: "${clientUserPoolWebClientId}",`);
-
-    const oauthDomainFQDN = `${cognitoExports[`Export${getExportName('userPoolDomain', {id: `spiadmin${envName}`})}`]}.auth.us-east-1.amazoncognito.com`;
-    idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)oauthDomain:.*/, `$1oauthDomain: "${oauthDomainFQDN}",`);
-
-    const clientWebAppFQDN = `https://${adminHostName}.${domainName}`;
-    idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)redirectSignIn:.*/, `$1redirectSignIn: "${clientWebAppFQDN}",`);
-    idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)redirectSignOut:.*/, `$1redirectSignOut: "${clientWebAppFQDN}",`);
-
-    const gatewayRestApiId = apiGatewayExports[`Export${getExportName('apiGatewayRestApiId')}`];
-    const gatewayStageName = apiGatewayExports[`Export${getExportName('apiGatewayDeploymentStage')}`];
-    const clientApiRootResourcePath = apiGatewayExports[`Export${getExportName('apiGatewayRootResourcePath')}`];
-    const apiPrefix = `${gatewayRestApiId}.execute-api.${awsRegion}.amazonaws.com`;
-    const stageRootResourcePath = `${gatewayStageName}${clientApiRootResourcePath}`;
-    idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)apiPrefix:.*/, `$1apiPrefix: "${apiPrefix}",`);
-    idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)stage:.*/, `$1stage: "${stageRootResourcePath}",`);
-
-    const pathToAdminUiBundle = "./services/adminUi"
-    fs.writeFileSync(`${pathToAdminUiBundle}/idpConfig.js`, idpConfigTemplateContents);
-
-    exec(`aws s3 cp ${pathToAdminUiBundle} s3://${adminUiS3BucketName}/admin_ui --recursive`, (err, output) => {
-      if (err) {
-        console.error("could not execute command: ", err)
-        return
-      }
-      console.log("Output: \n", output)
-    });
+exec(`aws s3 rm s3://${adminUiS3BucketName}/admin_ui`, (error, exists)=>{
+  if (error){
+    console.log("Error in removing existing admin UI contents. Uploading UI bundle failed.");
+    return;
   }
+  // Use IDP Config template to generate right idp config and replace idpConfig.js in adminUi bundle
+  let idpConfigTemplateContents = fs.readFileSync("./adminUiIdpConfigTemplate.js", "utf-8");
+
+  const cognitoStackExportsStartKey = 'PlatformPhaseSetupStackClientOnboardingUiStack';
+  const cognitoStackExportsKey = Object.keys(stackExports).filter(k => k.startsWith(cognitoStackExportsStartKey));
+  const cognitoExports = stackExports[cognitoStackExportsKey];
+
+  const apiGatewayStackExportsStartKey = 'PlatformPhaseSetupStackClientOnboardingUiStack';
+  const apiGatewayStackExportsKey = Object.keys(stackExports).filter(k => k.startsWith(apiGatewayStackExportsStartKey));
+  const apiGatewayExports = stackExports[apiGatewayStackExportsKey];
+
+  const clientUserPoolID = cognitoExports[`Export${getExportName('userPoolId', {id: `spiadmin${envName}`})}`];
+  const clientUserPoolWebClientId = cognitoExports[`Export${getExportName('userPoolClient', {id: `spiadmin${envName}`})}`];
+  idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)userPoolId:.*/, `$1userPoolId: "${clientUserPoolID}",`);
+  idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)userPoolWebClientId:.*/, `$1userPoolWebClientId: "${clientUserPoolWebClientId}",`);
+
+  const oauthDomainFQDN = `${cognitoExports[`Export${getExportName('userPoolDomain', {id: `spiadmin${envName}`})}`]}.auth.us-east-1.amazoncognito.com`;
+  idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)oauthDomain:.*/, `$1oauthDomain: "${oauthDomainFQDN}",`);
+
+  const clientWebAppFQDN = `https://${adminHostName}.${domainName}`;
+  idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)redirectSignIn:.*/, `$1redirectSignIn: "${clientWebAppFQDN}",`);
+  idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)redirectSignOut:.*/, `$1redirectSignOut: "${clientWebAppFQDN}",`);
+
+  const gatewayRestApiId = apiGatewayExports[`Export${getExportName('apiGatewayRestApiId')}`];
+  const gatewayStageName = apiGatewayExports[`Export${getExportName('apiGatewayDeploymentStage')}`];
+  const clientApiRootResourcePath = apiGatewayExports[`Export${getExportName('apiGatewayRootResourcePath')}`];
+  const apiPrefix = `${gatewayRestApiId}.execute-api.${awsRegion}.amazonaws.com`;
+  const stageRootResourcePath = `${gatewayStageName}${clientApiRootResourcePath}`;
+  idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)apiPrefix:.*/, `$1apiPrefix: "${apiPrefix}",`);
+  idpConfigTemplateContents = idpConfigTemplateContents.replace(/(.*)stage:.*/, `$1stage: "${stageRootResourcePath}",`);
+
+  const pathToAdminUiBundle = "./services/adminUi"
+  fs.writeFileSync(`${pathToAdminUiBundle}/idpConfig.js`, idpConfigTemplateContents);
+
+  exec(`aws s3 cp ${pathToAdminUiBundle} s3://${adminUiS3BucketName}/admin_ui --recursive`, (err, output) => {
+    if (err) {
+      console.error("could not execute command: ", err)
+      return
+    }
+    console.log("Output: \n", output)
+  });
 })
